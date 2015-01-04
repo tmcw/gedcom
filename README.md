4b6762ae66be339a871ced1011fec0062de2fc16
Tom MacWright <tom@macwright.org>
2015-01-04 15:42:42 -0500

    Works

--- README.md
+++ README.md
@@ -8,3 +8,21 @@ focuses on translating GEDCOM structure into [JSON](http://www.json.org/).
 * `parse(string, function(err, res) { })`
 * `.transformStream()`: returns a through stream that
   parses GEDCOM data.
+
+## CLI Usage
+
+`parse-gedcom` transforms GEDCOM files into JSON with proper nesting of
+family & child elements.
+
+```sh
+$ parse-gedcom < file.ged > output.json
+```
+
+`parse-gedcom-d3` transforms GEDCOM files into JSON ready to be used in
+a [d3](http://d3js.org/) force layout.
+
+```sh
+$ parse-gedcom-d3 < file.ged > output.json
+```
+
+See `demo.js` in this repository for an example.
--- d3ize.js
+++ d3ize.js
@@ -1,9 +1,10 @@
+function hasTag(val) {
+    return function(node) {
+        return node.tag === val;
+    };
+}
+
 function d3ize(tree) {
-    function hasTag(val) {
-        return function(node) {
-            return node.tag === val;
-        };
-    }
     var peopleNodes = tree
         .filter(hasTag('INDI'))
         .map(toNode);
@@ -12,22 +13,49 @@ function d3ize(tree) {
     var links = families.reduce(function(memo, family) {
         return memo.concat(familyLinks(family));
     }, []);
+    var allNodes = peopleNodes.concat(familyNodes);
+    var indexedNodes = allNodes.reduce(function(memo, node, i) {
+        memo[node.id] = i;
+        return memo;
+    }, {});
+    links = links.map(idToIndex(indexedNodes));
     return {
-        nodes: peopleNodes.concat(familyNodes),
+        nodes: allNodes,
         links: links
     };
 }
 
+function getName(p) {
+    if (p.tag === 'INDI') {
+        var nameNode = (p.tree.filter(hasTag('NAME')) || [])[0];
+        if (nameNode) {
+            return nameNode.data.replace(/\//g, '');
+        }
+    } else {
+        return 'Family';
+    }
+}
+
 function toNode(p) {
-    p.name = p.pointer;
+    p.id = p.pointer;
+    p.name = getName(p);
     return p;
+}
+
+function idToIndex(indexedNodes) {
+    return function(link) {
+        return {
+            source: indexedNodes[link.source],
+            target: indexedNodes[link.target]
+        };
+    };
 }
 
 function familyLinks(family) {
     var memberLinks = family.tree.map(function(member) {
         return {
-            from: family.pointer,
-            to: member.data
+            source: family.pointer,
+            target: member.data
         };
     });
     return memberLinks;
