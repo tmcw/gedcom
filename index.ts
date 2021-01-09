ca7ba6f79f8eaf5fa4f7e13f6a2f1b7d9562a9d4
Tom MacWright <tom@macwright.org>
2021-01-09 12:37:14 -0800

    Reorganize source into lib

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-export { parse } from "./parse-to-unist";
-export { tokenize } from "./tokenize";
-export { compact } from "./unist-compact";
-export { toD3Force } from "./to-d3-force";
-export { toDot } from "./to-dot";
+export { parse } from "./lib/parse-to-unist";
+export { tokenize } from "./lib/tokenize";
+export { compact } from "./lib/unist-compact";
+export { toD3Force } from "./lib/to-d3-force";
+export { toDot } from "./lib/to-dot";
+export { toGraphlib } from "./lib/to-graphlib";
--- lib/unist-compact.ts
+++ lib/unist-compact.ts
@@ -1,7 +1,13 @@
 import { Parent } from "unist";
 import visit from "unist-util-visit-parents";
 
+const remove = new Set(["TRLR", "SUBM", "HEAD"]);
+
 export function compact(root: Parent): Parent {
+  // Remove "trailer" objects, which are not useful to us.
+  root.children = root.children.filter((child) => {
+    return !remove.has(child.type);
+  });
   for (let child of root.children) {
     if (!child.data) child.data = {};
     visit(child, (node, ancestors) => {
